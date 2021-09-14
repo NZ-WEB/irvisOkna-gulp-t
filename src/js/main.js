ce268411eafc7fe67e8d5ce146b8846cd8a09e82
kann1ball <stereo00@yandex.ru>
2021-09-14 14:55:44 +0300

    init project

---
+++ src/js/main.js
@@ -0,0 +1,18 @@
+import "./slider";
+import modals from "./modules/modules.modal";
+import tabs from "./modules/module.tabs";
+import forms from  "./modules/module.forms";
+import changeModalState from "./modules/module.changeModalState";
+
+window.addEventListener('DOMContentLoaded', () => {
+    "use strict";
+
+    let modalState = {};
+
+    changeModalState(modalState);
+    modals();
+    tabs('.glazing_slider', '.glazing_block', '.glazing_content', 'active');
+    tabs('.decoration_slider', '.no_click', '.decoration_content > div > div', 'after_click');
+    tabs('.balcon_icons', '.balcon_icons_img', '.big_img > img', 'do_image_more', 'inline-block');
+    forms(modalState);
+});
---
+++ src/js/modules/module.changeModalState.js
@@ -0,0 +1,52 @@
+import checkNumInputs from "./module.checkNumInputs";
+
+const changeModalState = (state) => {
+    const windowForm = document.querySelectorAll('.balcon_icons_img'),
+        windowWidth = document.querySelectorAll('#width'),
+        windowHeight = document.querySelectorAll('#height'),
+        windowType = document.querySelector('#view_type'),
+        windowProfile = document.querySelectorAll('.checkbox');
+
+    checkNumInputs('#width');
+    checkNumInputs('#height');
+
+    function bindActionToElems (event, elem, prop) {
+        elem.forEach((item, idx) => {
+            item.addEventListener(event, () => {
+                switch (item.nodeName) {
+                    case 'SPAN' :
+                        state[prop] = idx;
+                        break
+                    case 'INPUT' :
+                        if (item.getAttribute('type') === 'checkbox') {
+                            idx === 0 ? state[prop] = "Холодное" : state[prop] = "Тёплое";
+                            elem.forEach((box, j) => {
+                                box.checked = false;
+                                if (idx = j) {
+                                    box.checked = true;
+                                }
+                            });
+                        } else {
+                            state[prop] = item.value;
+                        }
+                        break;
+                    case 'SELECT' :
+                        state[prop] = item.value;
+                        break;
+                }
+
+
+                console.log(state);
+            });
+        });
+    }
+
+    bindActionToElems('click', windowForm, 'form');
+    bindActionToElems('input', windowHeight, 'height');
+    bindActionToElems('input', windowWidth, 'Width');
+    bindActionToElems('change', windowType, 'type');
+    bindActionToElems('change', windowProfile, 'profile');
+
+};
+
+export default changeModalState;
